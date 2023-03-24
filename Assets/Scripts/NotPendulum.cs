7b1f3863f314b28a7c3185053cfe3f50e4324c4b
Justin3006 <75027185+Justin3006@users.noreply.github.com>
2023-03-24 14:24:07 +0100

    Bissln hübscherer Code

--- Assets/Scripts/NotPendulum.cs
+++ Assets/Scripts/NotPendulum.cs
@@ -2,26 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProtoProjectile : MonoBehaviour
+public class NotPendulum : MonoBehaviour
 {
     [SerializeField]
     int direction = 1;
     [SerializeField]
-    float speed = 10;
-    [SerializeField]
-    float rotation = 360;
+    float rotationSpeed = 360;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Translate(0,0, direction * speed * Time.fixedDeltaTime);
-        transform.Rotate(0, direction * rotation * Time.fixedDeltaTime, 0);
+        transform.Rotate(0, direction * rotationSpeed * Time.fixedDeltaTime, 0);
     }
 
     public void OnTriggerEnter(Collider other)

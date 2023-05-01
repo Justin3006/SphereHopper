fbb5929a7e99b0da22cde483c84acc1a6af361b6
Justin3006 <75027185+Justin3006@users.noreply.github.com>
2023-05-01 14:07:01 +0200

    begun implementation of an arena level

---
+++ Assets/Scripts/LevelGeneration/ArenaSpawn.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaSpawn : MonoBehaviour
+{
+    bool active;
+    bool cleared;
+    float size = 15;
+    List<GameObject[]> enemies;
+    List<GameObject> currentEnemies;
+    int spawnerDistance = 21;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if ((PlayerManager.GetTransform().position - transform.position).magnitude < size)
+            active = true;
+
+        if (active && !cleared) 
+        {
+            bool noEnemies = true;
+            foreach (GameObject enemy in currentEnemies) 
+            {
+                if (enemy != null)
+                    noEnemies = false;
+            }
+
+            if (!noEnemies) {
+                if (enemies.Count == 0)
+                {
+                    cleared = true;
+                    return;
+                }
+                currentEnemies.Clear();
+                GameObject[] enemiesToSpawn = enemies[0];
+                bool[] spawnUsed = new bool[4];
+
+                foreach (GameObject enemy in enemiesToSpawn)
+                {
+                    bool spawned = false;
+
+                    while (!spawned)
+                    {
+                        int rn = Random.Range(0, 4);
+                        if (spawnUsed[rn])
+                            continue;
+
+                        spawned = true;
+
+                        switch (rn)
+                        {
+                            case 0: currentEnemies.Add(Instantiate(enemy, new Vector3(-spawnerDistance, 0, 0), Quaternion.Euler(0, 0, 0))); break;
+                            case 1: currentEnemies.Add(Instantiate(enemy, new Vector3(spawnerDistance, 0, 0), Quaternion.Euler(0, 0, 0))); break;
+                            case 2: currentEnemies.Add(Instantiate(enemy, new Vector3(0, 0, -spawnerDistance), Quaternion.Euler(0, 0, 0))); break;
+                            case 3: currentEnemies.Add(Instantiate(enemy, new Vector3(0, 0, spawnerDistance), Quaternion.Euler(0, 0, 0))); break;
+                        }
+                    }
+                } 
+            }
+        }
+    }
+}

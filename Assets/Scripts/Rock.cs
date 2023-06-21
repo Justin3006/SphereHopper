bcac5dad8801604bc29501f026391a6b82b4478f
Justin3006 <75027185+Justin3006@users.noreply.github.com>
2023-06-21 15:31:16 +0200

    Small fix

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class Rock : MonoBehaviour
 {
+    bool adjusted = false;
+
+    private void FixedUpdate()
+    {
+        if (!adjusted)
+        {
+            RaycastHit hit;
+            Physics.Raycast(transform.position, -transform.up, out hit);
+
+            if (hit.collider != null) 
+            {
+                transform.Translate(0, hit.point.y - transform.position.y, 0);
+            }
+
+            adjusted = true;
+        }
+    }
+
     public void OnTriggerEnter(Collider other)
     {
         PlayerMotor p = other.gameObject.GetComponent<PlayerMotor>();
